dd2c7de23aac438d888132f494f8de75ad59aadc
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-07 12:53:56 +0000

    Item11808: revert perltidy on all lib/CPAN and all plugins which are CoordinateWithAuthor, as requested by Micha
    git-svn-id: http://svn.foswiki.org/trunk/TimelinePlugin@14764 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/TimelinePlugin.pm
+++ lib/Foswiki/Plugins/TimelinePlugin.pm
@@ -22,17 +22,17 @@
 package Foswiki::Plugins::TimelinePlugin;
 use strict;
 use vars qw( $VERSION $RELEASE $debug $pluginName $timelineId );
-$VERSION    = '$Rev$';
-$RELEASE    = 'Foswiki-1.0';
+$VERSION = '$Rev$';
+$RELEASE = 'Foswiki-1.0';
 $pluginName = 'TimelinePlugin';
 
+
 sub initPlugin {
-    my ( $topic, $web, $user, $installWeb ) = @_;
+    my( $topic, $web, $user, $installWeb ) = @_;
 
     # check for Plugins.pm versions
-    if ( $Foswiki::Plugins::VERSION < 1.026 ) {
-        Foswiki::Func::writeWarning(
-            "Version mismatch between $pluginName and Plugins.pm");
+    if( $Foswiki::Plugins::VERSION < 1.026 ) {
+        Foswiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
         return 0;
     }
 
@@ -40,7 +40,7 @@ sub initPlugin {
     Foswiki::Func::registerTagHandler( 'TIMELINE', \&_TIMELINE );
 
     # get debug flag
-    $debug = Foswiki::Func::getPreferencesFlag("\U$pluginName\E_DEBUG");
+    $debug = Foswiki::Func::getPreferencesFlag( "\U$pluginName\E_DEBUG" );
 
     # Plugin correctly initialized
     return 1;
@@ -49,8 +49,7 @@ sub initPlugin {
 # The function used to handle the %EXAMPLETAG{...}% variable
 # You would have one of these for each variable you want to process.
 sub _TIMELINE {
-    my ( $session, $params, $theTopic, $theWeb ) = @_;
-
+    my($session, $params, $theTopic, $theWeb) = @_;
     # $session  - a reference to the TWiki session object (if you don't know
     #             what this is, just ignore it)
     # $params=  - a reference to a Foswiki::Attrs object containing parameters.
@@ -65,47 +64,30 @@ sub _TIMELINE {
     # $params->{_DEFAULT} will be 'hamburger'
     # $params->{sideorder} will be 'onions'
     #add the JavaScript
-    my $pluginPubUrl =
-        Foswiki::Func::getPubUrlPath() . '/'
-      . Foswiki::Func::getTwikiWebname() . '/'
-      . $pluginName;
-    my $jscript = Foswiki::Func::readTemplate('timelineplugin');
+   my $pluginPubUrl = Foswiki::Func::getPubUrlPath().'/'.
+            Foswiki::Func::getTwikiWebname().'/'.$pluginName;    
+    my $jscript = Foswiki::Func::readTemplate ( 'timelineplugin' );
     $jscript =~ s/%PLUGINPUBURL%/$pluginPubUrl/g;
-    Foswiki::Func::addToHEAD( $pluginName, $jscript );
-
+    Foswiki::Func::addToHEAD($pluginName, $jscript);
+    
     my $orientation = 'Timeline.HORIZONTAL';
-
     #orientation="vertical"
-    if ( defined( $params->{orientation} )
-        and $params->{orientation} eq 'vertical' )
-    {
+    if (defined($params->{orientation}) and $params->{orientation} eq 'vertical') {
         $orientation = 'Timeline.VERTICAL';
     }
     my $urltype = 'JSON';
-
     #urltype="JSON"
-    if ( defined( $params->{urltype} ) and $params->{urltype} eq 'XML' ) {
+    if (defined($params->{urltype}) and $params->{urltype} eq 'XML') {
         $urltype = 'XML';
     }
 
-    my $timeline =
-        '<div id="my-timeline'
-      . $timelineId
-      . '" class="TimelineDiv" style="height: '
-      . $params->{height}
-      . '; width: '
-      . $params->{width}
-      . '; border: 1px solid #aaa" ' . 'url="'
-      . $params->{_DEFAULT} . '" '
-      . 'interval="'
-      . $params->{interval} . '" '
-      . 'orientation="'
-      . $orientation . '" '
-      . 'urltype="'
-      . $urltype . '" '
-      . 'date="'
-      . $params->{date}
-      . '"></div>';
+    my $timeline = '<div id="my-timeline'.$timelineId.'" class="TimelineDiv" style="height: '.$params->{height}
+        .'; width: '.$params->{width}.'; border: 1px solid #aaa" '
+        .'url="'.$params->{_DEFAULT}.'" '
+        .'interval="'.$params->{interval}.'" '
+        .'orientation="'.$orientation.'" '
+        .'urltype="'.$urltype.'" '
+        .'date="'.$params->{date}.'"></div>';
     $timelineId++;
     return $timeline;
 }
